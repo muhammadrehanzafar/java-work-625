d47de108ce0bcfc68883c03b61b504ce57a397db
Muhammad Rehan Zafar <muhammadrehanzafarmughal@gmail.com>
2019-03-03 20:41:17 +0500

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "This is Faheem to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
